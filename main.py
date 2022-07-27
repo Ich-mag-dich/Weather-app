97b4a7109019580b9213256566598401c8b91adc
asdasd <92120487+Ich-mag-dich@users.noreply.github.com>
2022-07-27 23:39:57 +0900

    bold: datetime, form data

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# uvicorn main:app --reload --host=0.0.0.0 --port=80
+# uvicorn main:app --reload --host=0.0.0.0 --port=8000
 
 from datetime import datetime
 from fastapi.responses import FileResponse
 from fastapi import FastAPI, Form
+
 app = FastAPI()
 
 # 메인페이지 접속시 "/" 로 가능
@@ -12,11 +13,11 @@ app = FastAPI()
 @app.get("/")
 def svJoin():
     nowTime = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    print(f'\n{nowTime}')
+    print(f"\n\033[1m{nowTime} \033[0m")
     return FileResponse("./html/index.html")
 
 
 @app.post("/send")
 async def sendData(username: str = Form(), password: str = Form()):
-    print(f"username: {username}, password: {password}")
+    print(f"\033[1m\nusername: {username}, password: {password} \033[0m")
     return {"username": username, "password": password}
