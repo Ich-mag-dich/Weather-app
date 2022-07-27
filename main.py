0ae7e1b94326dc2f9ee2ad2a232d94d932a0719b
asdasd <92120487+Ich-mag-dich@users.noreply.github.com>
2022-07-27 21:44:36 +0900

    index

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+from fastapi.responses import FileResponse
 from fastapi import FastAPI
 app = FastAPI()
 
 
 # 메인페이지 접속시 "/" 로 가능
-# 정확히는 나중에 알아보기
+# 정확히는 나중에 알아보기 -> 경로인가봄 ?
+# 아래 세 줄이 템플릿 느낌
+
 @app.get("/")
 def svJoin():
-    return "hello"
+    return FileResponse("./html/index.html")
+
+
+@app.get("/data")
+def svData():
+    return {"hello": 1234}
