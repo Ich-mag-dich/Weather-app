27231d278877687d215acac41156e23bbc017ec0
asdasd <92120487+Ich-mag-dich@users.noreply.github.com>
2022-07-27 23:04:46 +0900

    form post

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from fastapi.responses import FileResponse
-from fastapi import FastAPI
-app = FastAPI()
+# uvicorn main:app --reload --host=0.0.0.0 --port=80
 
+from datetime import datetime
+from fastapi.responses import FileResponse
+from fastapi import FastAPI, Form
+app = FastAPI()
 
 # 메인페이지 접속시 "/" 로 가능
 # 정확히는 나중에 알아보기 -> 경로인가봄 ?
 # 아래 세 줄이 템플릿 느낌
 
+
 @app.get("/")
 def svJoin():
+    nowTime = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    print(f'\n{nowTime}')
     return FileResponse("./html/index.html")
 
 
-@app.get("/data")
-def svData():
-    return {"hello": 1234}
+@app.post("/send")
+async def sendData(username: str = Form(), password: str = Form()):
+    print(f"username: {username}, password: {password}")
+    return {"username": username, "password": password}
